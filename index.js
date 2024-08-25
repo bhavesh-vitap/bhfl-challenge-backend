327cfe28f94edeea50b0b16af4711a0344c1e068
bhavesh-vitap <145153696+bhavesh-vitap@users.noreply.github.com>
2024-08-25 11:59:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use(cors());
 app.get('/bfhl', (req, res) => {
     res.status(200).json({
         "operation_code": 1
